47f941bfeb63f4837662b583fd6234b7e0d92fba
Danill Kravchenko <dankool175@gmail.com>
2021-04-30 02:55:54 +0300

    second part

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,25 +1,30 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9;
+    private Vector2D v1;
+
+    @BeforeAll
+    public void createNewVector(){
+        v1 = new Vector2D();
+    }
+
     @Test
     public void newVectorShouldHaveZeroLength(){
-        Vector2D v1 = new Vector2D(); // action
-
         // assertion
-        Assertions.assertEquals(0, v1.length(), 1e-9);
+        Assertions.assertEquals(0, v1.length(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroX(){
-        Vector2D v1 = new Vector2D();
-
-        Assertions.assertEquals(0, v1.getX(), 1e-9);
+        Assertions.assertEquals(0, v1.getX(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZero(){
-        Vector2D v1 = new Vector2D();
-        Assertions.assertEquals(0, v1.getY(), 1e-9);
+        Assertions.assertEquals(0, v1.getY(), EPS);
     }
+
 }
